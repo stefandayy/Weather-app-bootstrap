2769b0fa2301fdafae72777513dd8214cfed58ac
= <sted96@gmail.com>
2023-02-03 13:40:05 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,37 +5,6 @@ import axios from "axios";
 import { SpinningCircles } from "react-loading-icons";
 import FooterComponent from "./components/FooterComponent";
 
-/* const obj = {
-  coord: { lon: -96.7836, lat: 32.7668 },
-  weather: [{ id: 701, main: "Mist", description: "mist", icon: "50n" }],
-  base: "stations",
-  main: {
-    temp: 0.51,
-    feels_like: -2.97,
-    temp_min: -1.35,
-    temp_max: 1.83,
-    pressure: 1031,
-    humidity: 94,
-  },
-  visibility: 6437,
-  wind: { speed: 3.09, deg: 340 },
-  clouds: { all: 100 },
-  dt: 1675417729,
-  sys: {
-    type: 2,
-    id: 2075302,
-    country: "US",
-    sunrise: 1675430447,
-    sunset: 1675468863,
-  },
-  timezone: -21600,
-  id: 4684904,
-  name: "Dallas",
-  cod: 200,
-}; */
-
-//
-
 function App() {
   const [city, setCity] = useState("Firenze");
   const [weather, setWeather] = useState({});
